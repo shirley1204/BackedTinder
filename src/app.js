7891e07e362971ca409f9348a41e585e8cc39377
Shirleybutti <shirley.buti@fugetroncorp.com>
2024-10-14 18:31:42 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
-const express = require('express') //referecing express server from "node-modules"
-const app = express() // call the exprees() server 
+const express = require("express"); //referecing express server from "node-modules"
+const app = express(); // call the exprees() server
 
-// app.use("/",(req,res)=>{
-// res.send("Start page");
-// })
-
-app.use("/test",(req,res)=>{
-res.send("Hello from the server");
+//calling using use
+app.use("/user",(req,res)=>{
+    res.send("calling using use")
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server 2");
-    })
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Shirley",lastname:"Buti"})
+})
 
-    app.use("/hello1",(req,res)=>{
-        res.send("Hello from the server 3");
-        })
+//post call to /user
+app.post("/user",(req,res)=>{
+    console.log("saved data");
+    res.send("data saved successfully")
+})
 
-app.listen(3000,() =>{
-    console.log("server has successfully listening on port")
-}); // add port to listen 
+//this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello from the server 2");
+});
+
+app.use("/hello1", (req, res) => {
+  res.send("Hello from the server 3");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("Start page");
+// });
+
+app.listen(3000, () => {
+  console.log("server has successfully listening on port");
+}); // add port to listen
 //
