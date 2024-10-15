37e97f476454c41274ca1ab7fab3db89597ecbe7
Shirleybutti <shirley.buti@fugetroncorp.com>
2024-10-15 16:50:01 +0530

    next request handler

--- src/app.js
+++ src/app.js
@@ -1,26 +1,42 @@
 const express = require("express"); //referecing express server from "node-modules"
 const app = express(); // call the exprees() server
 
-//calling using use
-// app.use("/user",(req,res)=>{
-//     res.send("calling using use")
-// })
+// calling using use
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Hander 2");
+    next();
+    res.send("Handler 2 success");
+  },
+  (req, res, next) => {
+    console.log("3rd handler");
+    next();
+    console.log("err")
+  },(req,res,next)=>{
+    console.log("4th handler")
+  }
+);
 
 //This will only handle GET call to /user
-app.get("/user/:id/:name/:passward",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Shirley",lastname:"Buti"})
-})
+app.get("/user/:id/:name/:passward", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "Shirley", lastname: "Buti" });
+});
 
-app.get(/.*fly/,(req,res)=>{
-    res.send({firstname:"Shirley",lastname:"Buti"})
-})
+app.get(/.*fly/, (req, res) => {
+  res.send({ firstname: "Shirley", lastname: "Buti" });
+});
 
 //post call to /user
-app.post("/user",(req,res)=>{
-    console.log("saved data");
-    res.send("data saved successfully")
-})
+app.post("/user", (req, res) => {
+  console.log("saved data");
+  res.send("data saved successfully");
+});
 
 //this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
